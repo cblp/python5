de034aece55682f14d16ec032cf43ff07af6ed9e
Yuriy Syrovetskiy <cblp@cblp.su>
2015-03-15 23:45:27 +0300

    [+] __builtin__.all

--- src/Python5/Builtin.hs
+++ src/Python5/Builtin.hs
@@ -2,3 +2,6 @@ module Python5.Builtin where
 
 abs :: Num a => a -> a
 abs = Prelude.abs
+
+all :: [Bool] -> Bool
+all = Prelude.and
--- tests/TestsMain.hs
+++ tests/TestsMain.hs
@@ -13,3 +13,12 @@ main = hspec $ do
             Builtin.abs (-4 :: Double) `shouldBe` 4
         it "If the argument is a complex number, its magnitude is returned" $ do
             Builtin.abs (3 :+ 4) `shouldBe` 5
+
+    describe "__builtin__.all" $ do
+        it "returns True if all elements of the iterable are true" $ do
+            Builtin.all [True] `shouldBe` True
+            Builtin.all [False] `shouldBe` False
+            Builtin.all [True, True, True] `shouldBe` True
+            Builtin.all [True, True, False] `shouldBe` False
+        it "returns True if the iterable is empty" $ do
+            Builtin.all [] `shouldBe` True
