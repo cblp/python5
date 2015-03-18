63fee4f1fabc21a663a79c61b79752443ea31309
Yuriy Syrovetskiy <cblp@cblp.su>
2015-03-18 21:56:15 +0300

    [+] example of calc

---
+++ examples/calc.hs
@@ -0,0 +1,28 @@
+{-
+    Intuitive Interpretation
+
+    Calculations are simple with Python,
+    and expression syntax is straightforward:
+    the operators +, -, * and / work as expected;
+    parentheses () can be used for grouping.
+
+    # Python 3: Simple arithmetic
+    >>> 1 / 2
+    0.5
+    >>> 2 ** 3
+    8
+    >>> 17 / 3  # classic division returns a float
+    5.666666666666667
+    >>> 17 // 3  # floor division
+    5
+-}
+
+main = do
+    print(1 / 2)
+    -- 0.5
+    print(2 ^ 3)
+    -- 8
+    print(17 / 3) -- classic division returns a float
+    -- 5.666666666666667
+    print(17 `div` 3) -- floor division
+    -- 5
