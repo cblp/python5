b80be491fe8283164598c7f40fc18a9af7474d98
Yuriy Syrovetskiy <cblp@cblp.su>
2015-03-18 22:07:47 +0300

    [+] example of IO

---
+++ examples/io.hs
@@ -0,0 +1,26 @@
+input :: String -> IO String
+input prompt = do
+    putStr prompt
+    getLine
+
+--------------------------------------------------------------------------------
+
+{-
+    Quick & Easy to Learn
+
+    Experienced programmers in any other language
+    can pick up Python very quickly,
+    and beginners find the clean syntax and indentation structure easy to learn.
+-}
+
+main = do
+    -- Python 3: Simple output (with Unicode)
+    print("Hello, I'm Python5!")
+    -- Hello, I'm Python5!
+
+    -- Input, assignment
+    name <- input("What is your name?\n")
+    print("Hi, ", name, ".")
+    -- What is your name?
+    -- Python5
+    -- Hi, Python5.
