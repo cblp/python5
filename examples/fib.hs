f1a1b58a26c26e909271d1a19e4a2c94dce98dc2
Yuriy Syrovetskiy <cblp@cblp.su>
2015-03-18 21:39:13 +0300

    [+] more docs for example

--- examples/fib.hs
+++ examples/fib.hs
@@ -55,15 +55,22 @@ instance Str Integer where
 
 --------------------------------------------------------------------------------
 
-{- # Python 3: Fibonacci series up to n
->>> def fib(n):
->>>     a, b = 0, 1
->>>     while a < n:
->>>         print(a, end=' ')
->>>         a, b = b, a+b
->>>     print()
->>> fib(1000)
-0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987
+{-
+    Functions Defined
+
+    The core of extensible programming is defining functions.
+    Python allows mandatory and optional arguments, keyword arguments,
+    and even arbitrary argument lists.
+
+    # Python 3: Fibonacci series up to n
+    >>> def fib(n):
+    >>>     a, b = 0, 1
+    >>>     while a < n:
+    >>>         print(a, end=' ')
+    >>>         a, b = b, a+b
+    >>>     print()
+    >>> fib(1000)
+    0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987
 -}
 
 fib(n :: Integer) = do
