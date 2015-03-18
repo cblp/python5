c086c300d89f16ab4bfdf7f7304e5c3e3b523d85
Yuriy Syrovetskiy <cblp@cblp.su>
2015-03-18 21:50:57 +0300

    [+] example on data

---
+++ examples/data.hs
@@ -0,0 +1,41 @@
+module Main (main) where
+
+import Data.Char as Char
+
+string_upper :: String -> String
+string_upper = map Char.toUpper
+
+list :: [a] -> [a]
+list = id
+
+enumerate :: [a] -> [(Integer, a)]
+enumerate = zip [0 :: Integer ..]
+
+--------------------------------------------------------------------------------
+
+{-
+    Compound Data Types
+
+    Lists (known as arrays in other languages) are
+    one of the compound data types that Python understands.
+    Lists can be indexed, sliced and manipulated with other built-in functions.
+
+    # Python 3: List comprehensions
+    >>> fruits = ['Banana', 'Apple', 'Lime']
+    >>> loud_fruits = [fruit.upper() for fruit in fruits]
+    >>> print(loud_fruits)
+    ['BANANA', 'APPLE', 'LIME']
+
+    # List and the enumerate function
+    >>> list(enumerate(fruits))
+    [(0, 'Banana'), (1, 'Apple'), (2, 'Lime')]
+-}
+
+main = do
+    let fruits = ["Banana", "Apple", "Lime"]
+    let loud_fruits = [string_upper fruit | fruit <- fruits]
+    print(loud_fruits)
+    -- ["BANANA", "APPLE", "LIME"]
+
+    print(list(enumerate(fruits)))
+    -- [(0, "Banana"), (1, "Apple"), (2, "Lime")]
