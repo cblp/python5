9a87d6a7d84628256abd6e67b27df00ccaf38a96
Yuriy Syrovetskiy <cblp@cblp.su>
2015-03-21 12:08:18 +0300

    [m] tests in modules

---
+++ tests/Builtin.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Builtin where
+
+import Python5.Builtin
+import Test.Hspec
+
+spec :: Spec
+spec = do
+    describe "__builtin__.abs" $ do
+        it "returns the absolute value of a number" $ do
+            abs(int(1)) `shouldBe` 1
+            abs(int(-2)) `shouldBe` 2
+        it "The argument may be an integer or a floating point number" $ do
+            abs(int(3)) `shouldBe` 3
+            abs(float(-4)) `shouldBe` 4
+        it "If the argument is a complex number, its magnitude is returned" $
+            abs(complex(3, 4)) `shouldBe` float(5)
+
+    describe "__builtin__.all" $ do
+        it "returns True if all elements of the iterable are true" $ do
+            all [True] `shouldBe` True
+            all [False] `shouldBe` False
+            all [True, True, True] `shouldBe` True
+            all [True, True, False] `shouldBe` False
+        it "returns True if the iterable is empty" $
+            all [] `shouldBe` True
--- tests/Main.hs
+++ tests/Main.hs
@@ -16,28 +16,11 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -}
 
-{-# LANGUAGE NoImplicitPrelude #-}
-
-import Python5.Builtin
 import Test.Hspec
 
-main :: Proc
-main = hspec $ do
-    describe "__builtin__.abs" $ do
-        it "returns the absolute value of a number" $ do
-            abs(int(1)) `shouldBe` 1
-            abs(int(-2)) `shouldBe` 2
-        it "The argument may be an integer or a floating point number" $ do
-            abs(int(3)) `shouldBe` 3
-            abs(float(-4)) `shouldBe` 4
-        it "If the argument is a complex number, its magnitude is returned" $ do
-            abs(complex(3, 4)) `shouldBe` float(5)
+-- tests
+import qualified Builtin
 
-    describe "__builtin__.all" $ do
-        it "returns True if all elements of the iterable are true" $ do
-            all [True] `shouldBe` True
-            all [False] `shouldBe` False
-            all [True, True, True] `shouldBe` True
-            all [True, True, False] `shouldBe` False
-        it "returns True if the iterable is empty" $ do
-            all [] `shouldBe` True
+main :: IO ()
+main = hspec
+    Builtin.spec
